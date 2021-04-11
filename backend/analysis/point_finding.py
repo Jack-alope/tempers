bc035156e3ba2d567e512636eaf79ea25cffb4c5
Jack <jack@mrph.dev>
2021-04-11 15:55:02 +0100

    good preogress

--- backend/analysis/point_finding.py
+++ backend/analysis/point_finding.py
@@ -1,12 +1,37 @@
+'''This module is responsible for finding relevant points used in analysis'''
+
 import peakutils
+import sys 
 
 def find_peaks(time, disp):
-    """Finds usable peaks"""
+    ''' Finds usable peaks '''
     unformatted_peaks = peakutils.indexes(disp, .1, .10)[1:-1]
     peaks = (time[unformatted_peaks], disp[unformatted_peaks], unformatted_peaks)
     return peaks
 
-def find_basepoints(time, disp, peaks):
-    peak_x, peak_y, indicies = peaks
+def find_basepoints_frontpoints(disp, peaks):
+    '''Use the dfdt_recursive func to find basepoints and frontpointspyl'''
+    peak_indicies = peaks[2]
+    basepoints = [dfdt_recursive(disp, peak_index, lambda x:x-1) for peak_index in peak_indicies]
+    frontpoints = [dfdt_recursive(disp, peak_index, lambda x:x+1) for peak_index in peak_indicies]
+    return (basepoints, frontpoints)
 
-    return indicies
+def find_analysispoints(disp, peaks, base_values):
+    base_index = base_values[1]
+    peak_index = peaks[2][1]
+    ind = disp_recursive(disp, peak_index, base_index, lambda x:x-1, .1)
+    
+    return ind
+
+def dfdt_recursive(disp, peak_index, incrementor):
+    ''' Recursively moves along graph until it changes direction '''
+    new_index = incrementor(peak_index)
+    if disp[new_index] - disp[peak_index] > 0:
+        return peak_index
+    return dfdt_recursive(disp, new_index, incrementor)
+
+def disp_recursive(disp, peak_index, base_index, incrementor, percentage):
+    new_index = incrementor(peak_index)
+    if disp[new_index] - disp[peak_index] < percentage * (disp[base_index] - disp[peak_index]):
+        return new_index
+    return disp_recursive(disp, new_index, base_index, incrementor, percentage)
--- tests/test_point_finding.py
+++ tests/test_point_finding.py
@@ -5,30 +5,41 @@ import numpy as np
 import matplotlib.pyplot as plot
 import pytest
 
+import sys
+sys.setrecursionlimit(10000000) 
+
 @pytest.fixture(name='time')
 def fixture_time():
     """Time arrays used for testing"""
-    return np.arange(0, 80, 0.01)
+    return np.arange(0, 40, 0.001)
 
 @pytest.fixture(name='disp')
 def fixture_disp(time):
     """Displacement arrays used for testing"""
-    return np.sin(time)
+    return np.sin(3*time)
 
 @pytest.fixture(name='peaks')
 def fixture_peaks(time, disp):
     """Peak arrays used for testing"""
     return point_finding.find_peaks(time, disp)
 
+@pytest.fixture(name='base_indicies')
+def fixture_base_indicies(disp, peaks):
+    return point_finding.find_basepoints_frontpoints(disp, peaks)[0]
+
 def test_find_peaks(time, disp, peaks):
     """Testing peaks"""
-    # plot.plot(time, disp);plot.plot(peaks[0], peaks[1]);plot.show()
-    assert np.all([.9999 <= amplitude <= 1.0001 for amplitude in peaks[1]])
+    #plot.plot(time, disp);plot.plot(peaks[0], 0*peaks[1]);plot.show()
+    #assert np.all([1.569 <= time % np.pi <= 1.571 for time in peaks[0]])
+    assert np.all([.99 <= amplitude <= 1.01 for amplitude in peaks[1]])
     return 
 
-def test_find_basepoints(time, disp, peaks):
+def test_find_basepoints_frontpoints(time, disp, peaks):
     """Return the pathname of the KOS root directory."""
-    ind = point_finding.find_basepoints(time, disp, peaks)
-    plot.plot(time, disp);plot.plot(time[ind], disp[ind]);plot.show()
+    ind, frontpoints = point_finding.find_basepoints_frontpoints(disp, peaks)
     return 6
 
+def test_find_analysispoints(time, disp, peaks, base_indicies):
+    inds = point_finding.find_analysispoints(disp, peaks, base_indicies)
+    plot.plot(time, disp);plot.plot(time[inds], disp[inds], marker='o');plot.show()
+    return
