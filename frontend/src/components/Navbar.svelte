f4ac030d5f8df9fd33786588966455e9da33d3cf
thesimplekid <brendan@thesimplekid.com>
2021-04-18 14:00:21 -0400

    change name

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -5,7 +5,7 @@
 <nav class="bg-blue-700 shadow-lg">
   <div class="container mx-auto">
     <div class="flex justify-between">
-      <a href="/" class="text-white text-3xl font-bold p-3">MTT Analysis</a>
+      <a href="/" class="text-white text-3xl font-bold p-3">Rianú</a>
       <ul class="text-gray-400 sm:self-center text-xl border-t sm:border-none">
         <li class="sm:inline-block">
           <a href="/" class="p-3 text-white hover:text-black">Home</a>
@@ -29,8 +29,9 @@
           >
         </li>
         <li class="sm:inline-block">
-          <a href="https://mtt.mrph.dev" class="p-3 text-white hover:text-black"
-            >Docs</a
+          <a
+            href="https://rianu.mrph.dev/"
+            class="p-3 text-white hover:text-black">Docs</a
           >
         </li>
       </ul>
